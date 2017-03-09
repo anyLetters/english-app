eb1600328b78927046af3b59ac7715f6096e245d
anyLetters <maximko999@yahoo.com>
2017-03-09 17:17:01 +0500

    another style update

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -6,45 +6,21 @@ $(document).ready(function(){
   sidebar = $('.sidebar-search');
   toggle_button = $('.options-search-button');
   toggle_button.click(function() {
-    sidebar.toggle(function(){
-    // sidebar.animate({width:auto});
-    });
+    sidebar.slideToggle();
   });
 });
 
-var countWatchedWords = function () {
-  
-  countViewsReset();
+var voiceText = function (element) {
 
-  if (localStorage.views) {
-    localStorage.views = Number(localStorage.views) + 1;
-  } else {
-    localStorage.views = 1;
-  }
+  if (typeof element !== 'undefined')
+    var text = document.getElementsByClassName(element.className)[0].innerHTML;
+  else
+    var text = document.getElementsByClassName("word-eng")[0].innerHTML;
 
-  setTimeout(function () { 
-    document.getElementsByClassName("info-views")[0].innerHTML = "You have watched " +
-    localStorage.views + " words today";
-  }, 250);
-
-};
-
-var voiceText = function () {
-
-  if (document.getElementsByClassName("word-eng").length > 0) {
-    var valueElem = document.getElementsByClassName("word-eng")[0].innerHTML;
-  }
-
-  // if (document.getElementsByClassName("show-word").length > 0) {
-  //   var valueElem = document.getElementsByClassName("show-word")[0].innerHTML;
-  // }
-
-  if (valueElem !== 'undefined' && valueElem !== '') {
-    responsiveVoice.speak(valueElem, "US English Male");
-
-  } else {
+  if (typeof text !== 'undefined' && text !== '')
+    responsiveVoice.speak(text, "US English Male");
+  else
     console.log('error');
-  }
 
 };
 
@@ -61,9 +37,24 @@ var countViewsReset = function () {
   var currentDay = date.getDate;
   currentDay = currentDay.call(date);
 
-  if (parseInt(localStorage.day) !== currentDay) {
+  if (parseInt(localStorage.day) !== currentDay)  
     localStorage.clear();
-  }
+
+};
+
+var countWatchedWords = function () {
+  
+  countViewsReset();
+
+  if (localStorage.views)
+    localStorage.views = Number(localStorage.views) + 1;
+  else
+    localStorage.views = 1;
+
+  setTimeout(function () { 
+    document.getElementsByClassName("info-views")[0].innerHTML = "You have watched " +
+    localStorage.views + " words today";
+  }, 250);
 
 };
 
